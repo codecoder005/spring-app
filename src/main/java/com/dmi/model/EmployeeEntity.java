3ebfad7f17f1632bd81f6d41e448ce98d25d065a
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-02-11 09:36:50 +0530

    lombok props added to employee entity

--- src/main/java/com/dmi/model/EmployeeEntity.java
+++ src/main/java/com/dmi/model/EmployeeEntity.java
@@ -1,4 +1,18 @@
 package com.dmi.model;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Data
+@Builder
 public class EmployeeEntity {
+    private Integer empId;
+    private String firstName;
+    private String email;
 }
